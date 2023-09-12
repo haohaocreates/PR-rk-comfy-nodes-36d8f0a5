71a02c39f522cb9502adae347485755578bd47ed
Rainer Klaffehn <rainer.klaffehn@gmx.de>
2023-09-12 17:39:55 +0200

    Use `lru_cache` from `functools`

--- nodes/RK_CivitAINodes.py
+++ nodes/RK_CivitAINodes.py
@@ -3,27 +3,7 @@ import folder_paths
 import hashlib
 import os
 
-from collections import OrderedDict
-
-
-class LRUCache:
-    def __init__(self, capacity: int):
-        self.capacity = capacity
-        self.cache = OrderedDict()
-
-    def get(self, key):
-        if key in self.cache:
-            self.cache.move_to_end(key)
-            return self.cache[key]
-        return None
-
-    def put(self, key, value):
-        if key in self.cache:
-            self.cache.move_to_end(key)
-        elif len(self.cache) >= self.capacity:
-            self.cache.popitem(last=False)
-        self.cache[key] = value
-
+from functools import lru_cache
 
 class RK_CivitAIMetaChecker:
     '''Parse the prompt (image meta-data) and verify that CivitAI might find the
@@ -64,8 +44,6 @@ class RK_CivitAIAddHashes:
     OUTPUT_NODE = True
     CATEGORY = "RK_Nodes/image"
 
-    _SHA_CACHE = LRUCache(10)
-
     def execute(self, image, prompt: dict = None):
         for id, node in prompt.items():
             try:
@@ -87,16 +65,14 @@ class RK_CivitAIAddHashes:
                 model_path = os.path.normpath(
                     folder_paths.get_full_path(MODEL_PARAMETERS[parameter], value))
 
-                digest = self._SHA_CACHE.get(model_path)
-                if digest is None:
-                    digest = self._computeFileSha(model_path)
-                    self._SHA_CACHE.put(model_path, digest)
+                digest = self._computeFileSha(model_path)
 
                 hash_key = str(parameter).replace("_name", "_hash")
                 print(f"{model_path}: assign sha256 {digest} as {hash_key}")
                 node.update({hash_key: digest})
 
     @staticmethod
+    @lru_cache(maxsize=10)
     def _computeFileSha(model_path: str) -> str:
         with open(model_path, "rb") as model_file:
             make_hash = hashlib.sha256()
